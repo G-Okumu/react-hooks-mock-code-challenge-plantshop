92b61abc1e5f2bd7e386b410a1433e2b2ef92413
George-Okumu <gokumu12@gmail.com>
2022-04-18 17:28:06 +0300

    added search functionality

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,38 +1,44 @@
-import React, { useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
   const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
 
   useEffect(() => {
     fetch("http://localhost:6001/plants")
-    .then(response => response.json())
-    .then(data => {
-      setPlants(data);
-    })
+      .then(response => response.json())
+      .then(data => {
+        setPlants(data);
+      })
   }, [])
 
 
-    //Delete single plant
-    function deleteSinglePlant(id, name) {
-      fetch(`http://localhost:6001/plants/${id}`, {
-        method: 'DELETE'
-      }).then(response => {
-        return response.json()
-      }).then(() => {
-        const newPlants = plants.filter((plant) => plant.id !== id);
-        setPlants(newPlants);
-      });
-      alert(`${name} deleted successfully`);
-    }
+  //For search
+  const searchedPlant = plants.filter((plant) => plant.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+  //Delete single plant
+  function deleteSinglePlant(id, name) {
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: 'DELETE'
+    }).then(response => {
+      return response.json()
+    }).then(() => {
+      const newPlants = plants.filter((plant) => plant.id !== id);
+      setPlants(newPlants);
+    });
+    alert(`${name} deleted successfully`);
+  }
+
 
   return (
     <main>
       <NewPlantForm />
-      <Search />
-      <PlantList plants={plants} deletePlant={deleteSinglePlant} />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <PlantList plants={searchedPlant} deletePlant={deleteSinglePlant} />
     </main>
   );
 }
